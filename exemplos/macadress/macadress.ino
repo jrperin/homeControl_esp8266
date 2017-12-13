bb393152e2ad0efe17ebb7632c97b08d7fc51489
Joao Roberto Perin <jrperin@gmail.com>
2017-12-13 20:56:56 -0200

    Update macadress.ino

--- exemplos/macadress/macadress.ino
+++ exemplos/macadress/macadress.ino
@@ -1,7 +1,7 @@
 #include <ESP8266WiFi.h>
 
 uint8_t MAC_array[6];
-char MAC_char[18];
+char MAC_char[18]; // Exemplo: 00:19:B9:FB:E2:57
 
 void setup() {
     Serial.begin(115200);
@@ -9,6 +9,7 @@ void setup() {
 
     WiFi.macAddress(MAC_array);
     for (int i = 0; i < sizeof(MAC_array); ++i){
+      //Converte os campos de byte em hexa
       sprintf(MAC_char,"%s%02x:",MAC_char,MAC_array[i]);
     }
   
